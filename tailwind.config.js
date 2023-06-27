19afae2834470e12e518380b491946324f77a6df
e.shkriada <e.shkryada@gmail.com>
2023-06-27 12:14:36 +0400

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,53 @@
 module.exports = {
   content: ['./src/**/*.html'],
   theme: {
+    fontFamily: {
+      poppins: 'Poppins',
+    },
+    fontSize: {
+      xs: [
+        '14px',
+        {
+          fontWeight: 700,
+          lineHeight: '21px',
+        },
+      ],
+      sm: [
+        '16px',
+        {
+          lineHeight: '24px',
+          fontWeight: 400,
+        },
+      ],
+      base: [
+        '18px',
+        {
+          fontWeight: 500,
+          lineHeight: '27px',
+        },
+      ],
+      lg: [
+        '32px',
+        {
+          fontWeight: 700,
+          lineHeight: '48px',
+        },
+      ],
+      xl: [
+        '48px',
+        {
+          fontWeight: 700,
+          lineHeight: '72px',
+        },
+      ],
+      '2xl': [
+        '62px',
+        {
+          fontWeight: 800,
+          lineHeight: '93px',
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [],
