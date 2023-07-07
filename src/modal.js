a52ffad703ee7c801cbe0f949c69e78ca239beb3
e.shkriada <e.shkryada@gmail.com>
2023-07-07 15:56:54 +0400

    Update layout

--- src/modal.js
+++ src/modal.js
@@ -1,52 +1,48 @@
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
+const dialog = document.getElementById('dialog');
+const bodyEl = document.querySelector('body');
 const openModalBtn = document.querySelector('.btn-open');
-const closeModalBtn = document.querySelector('.btn-close');
-const links = document.querySelectorAll('a.modal-link');
+const closeModalBtn = document.querySelectorAll('.btn-close');
+
+const menuBtns = document.querySelectorAll('.modal-menu-btn');
 const about = document.querySelector('#about');
 const contactUs = document.querySelector('#contact');
 const modalBtn = document.querySelector('#modal-btn');
 
-// close modal function
 const closeModal = function () {
-  modal.classList.add('modal-hidden');
-  overlay.classList.add('hidden');
+  dialog.close();
+  bodyEl.classList.toggle('noscroll');
 
   // clear state of modal content
-  links[0].classList.add('active');
-  links[1].classList.remove('active');
+  menuBtns[0].classList.add('active');
+  menuBtns[1].classList.remove('active');
   modalBtn.innerHTML = "Let's connect";
   contactUs.classList.add('hidden');
   about.classList.remove('hidden');
 };
 
-// close the modal when the close button and overlay is clicked
-closeModalBtn.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+closeModalBtn.forEach((btn) => btn.addEventListener('click', closeModal));
 
-// close modal when the Esc key is pressed
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  if (e.key === 'Escape') {
     closeModal();
   }
 });
 
-// open modal function
 const openModal = function () {
-  modal.classList.remove('modal-hidden');
-  overlay.classList.remove('hidden');
+  dialog.showModal();
+  bodyEl.classList.toggle('noscroll');
 };
-// open modal event
+
 openModalBtn.addEventListener('click', openModal);
 
 // change inner modal content
-for (let link of links) {
-  link.addEventListener('click', (e) => {
+for (let btn of menuBtns) {
+  btn.addEventListener('click', (e) => {
     const el = e.target;
 
     if (el.classList.contains('active')) return;
 
-    const activeNow = document.querySelector('a.active');
+    const activeNow = document.querySelector('.active');
 
     if (activeNow.innerHTML === 'About Us') {
       modalBtn.innerHTML = 'Message us';
